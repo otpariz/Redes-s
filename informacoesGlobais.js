73409d86402392c3e840500e6f89a3b3179ac400
otávio <114525486+otpariz@users.noreply.github.com>
2024-09-30 10:24:31 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -4,6 +4,14 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
+    const paragrafo document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = ´Vocẽ sabia que o mundo ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${total_pessoas_conectadas}
+    estão conectadas em alguma rede social e passam em média ${dados.tempo_médio} horas conectadas. ´
+    console.log(paragrafo)
+
+    const container = document.getElementByid(´graficos-container´)
+    container.appendChild(paragrafo)
 }
 
 vizualizarInformacoesGlobais()
