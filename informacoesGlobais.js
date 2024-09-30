dec40743f8ce0f1ccb46ee7e4b4b51e00c8abbea
otávio <114525486+otpariz@users.noreply.github.com>
2024-09-30 10:46:41 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -6,8 +6,8 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados);
     const paragrafo document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = ´Vocẽ sabia que o mundo ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${total_pessoas_conectadas}
-    estão conectadas em alguma rede social e passam em média ${dados.tempo_médio} horas conectadas. ´
+    paragrafo.innerHTML = ´Vocẽ sabia que o mundo <span>${dados.total_pessoas_mundo}<\span> de pessoas e que aproximadamente <span>${total_pessoas_conectadas}<\span>
+    estão conectadas em alguma rede social e passam em média <span>${dados.tempo_médio}<\span> horas conectadas. ´
     console.log(paragrafo)
 
     const container = document.getElementByid(´graficos-container´)
